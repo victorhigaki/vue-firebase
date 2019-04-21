92519f1001d14bbed3e3ab72c0b5342715f17de4
Victor Higaki <higvic@gmail.com>
2019-04-20 22:59:54 -0300

    Vue CLI

---
+++ Section 3 The Vue CLI/21. Passing Data with Props 22. Custom Events/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <Navbar/>
+    <AllFriends :friends="friends" @delete="deleteFriend"/>
+    <OnlineFriends :friends="friends"/>
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar";
+import AllFriends from "./AllFriends";
+import OnlineFriends from "./OnlineFriends";
+
+export default {
+  name: "app",
+  components: {
+    Navbar,
+    OnlineFriends,
+    AllFriends
+  },
+  data() {
+    return {
+      title: "My first Vue app, wooo :)",
+      friends: [
+        { name: "Mario", online: true },
+        { name: "Luigi", online: false },
+        { name: "Toad", online: true },
+        { name: "Browser", online: false }
+      ]
+    };
+  },
+  methods: {
+    deleteFriend(payload) {
+      console.log(payload);
+      this.friends = this.friends.filter(friend => {
+        return friend.name !== payload.name;
+      });
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  color: #444;
+  font-weight: normal;
+  text-align: center;
+}
+</style>
